c0ff41749526ec3f0460b0d2d3713effda676b4c
Mateusz Sęk <s34002@pjwstk.edu.pl>
2026-03-11 09:39:31 +0100

    added user input

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
 ﻿using apbd_cw1_git_s34002;
+
 class Program
 {
     static void Main()
     {
-        int[] numbers = { 5, 10, 15, 20 };
+        Console.WriteLine("wprowadz lizcby oddzielone spacja, aby obliczyc z nich srednia:");
+
+        string input = Console.ReadLine();
+
+        int[] numbers = input
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
         double average = StatisticsHelper.CalculateAverage(numbers);
+
         Console.WriteLine($"srednia: {average}");
     }
 }
