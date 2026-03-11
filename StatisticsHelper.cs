a07948e18fe3ab90c02757077f945aa7acbb483a
Mateusz Sęk <s34002@pjwstk.edu.pl>
2026-03-11 09:47:29 +0100

    added sum calculation

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -10,4 +10,16 @@ public static class StatisticsHelper
         }
         return (double)sum / numbers.Length;
     }
+    
+    public static int CalculateSum(int[] values)
+    {
+        int sum = 0;
+
+        foreach (int value in values)
+        {
+            sum += value;
+        }
+
+        return sum;
+    }
 }
