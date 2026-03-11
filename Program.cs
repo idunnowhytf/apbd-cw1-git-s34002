70154880cd0d9de14c28c16578a246bdf2b15941
Mateusz Sęk <s34002@pjwstk.edu.pl>
2026-03-11 09:34:07 +0100

    added basic calculation of average

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 5, 10, 15, 20 };
+        double average = CalculateAverage(numbers);
+        Console.WriteLine($"srednia: {average}");
+    }
+
+    static double CalculateAverage(int[] numbers)
+    {
+        int sum = 0;
+        foreach (int n in numbers)
+        {
+            sum += n;
+        }
+        return (double)sum / numbers.Length;
+    }
+}
