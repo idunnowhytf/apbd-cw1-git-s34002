bcbb464d0534b7fd5c1ba12ecfce9461c4790e25
Mateusz Sęk <s34002@pjwstk.edu.pl>
2026-03-17 17:53:06 +0100

    changed output message on main branch

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine("enter numbers separated by space:");
 string input = Console.ReadLine();
 int[] numbers = input.Split(' ').Select(int.Parse).ToArray();
 
-Console.WriteLine($"Average: {StatisticsHelper.CalculateAverage(numbers)}");
+Console.WriteLine("Calculated statistics:");
 Console.WriteLine($"MAX: {StatisticsHelper.CalculateMax(numbers)}");
 Console.WriteLine($"MIN: {StatisticsHelper.CalculateMin(numbers)}");
 if (numbers.Length == 0)
