de4256e3085a0ae0d855f18efca81e67f9d43f42
Mateusz Sęk <s34002@pjwstk.edu.pl>
2026-03-17 17:43:13 +0100

    Added basic user input

--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
 ﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("enter numbers separated by space:");
+string input = Console.ReadLine();
+int[] numbers = input.Split(' ').Select(int.Parse).ToArray();
