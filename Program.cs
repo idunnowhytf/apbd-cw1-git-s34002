9ae0d8b5db8a137c9b481ce066d9e752337258a6
Mateusz Sęk <s34002@pjwstk.edu.pl>
2026-03-17 17:47:32 +0100

    added usage of CalculateAverage in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using apbd_cw1_git_s34002;
+
+Console.WriteLine("Hello, World!");
 Console.WriteLine("enter numbers separated by space:");
 string input = Console.ReadLine();
 int[] numbers = input.Split(' ').Select(int.Parse).ToArray();
+
+Console.WriteLine($"Average: {StatisticsHelper.CalculateAverage(numbers)}");
 
 if (numbers.Length == 0)
 {
